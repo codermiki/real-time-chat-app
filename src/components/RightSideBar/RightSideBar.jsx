c4a9debfa69cdc700c1d09798f445378e614f528
codermiki <mikiastadessemiki@gmail.com>
2025-07-13 22:48:42 +0300

    feat: enhance RightSideBar with profile display and styling, add Logout button

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 import styles from "./RightSideBar.module.css";
+import Button from "../Button/Button";
 
 function RightSideBar() {
    return (
       <>
-         <h2>Right Side Bar</h2>
+         <div className={`${styles["right-sidebar"]}`}>
+            <div>
+               <div className={`${styles["profile"]}`}>
+                  <img
+                     className={`${styles["profile_img"]}`}
+                     src="https://picsum.photos/200/300"
+                     alt=""
+                  />
+                  <div className={`${styles["profile_info"]}`}>
+                     <p className={`${styles["profile_status"]}`}></p>
+                     <p className={`${styles["profile_name"]}`}>
+                        Caroline Gray
+                     </p>
+                  </div>
+                  <p className={`${styles["profile_bio"]}`}>
+                     Lorem ipsum is placeholder text commonly used in ..
+                  </p>
+               </div>
+               <hr className={`${styles["hr"]}`} />
+               <div className={`${styles["media"]}`}>
+                  <div>media</div>
+                  <div className={`${styles["media_container"]}`}>
+                     <img
+                        className={`${styles["media_content"]}`}
+                        src="https://picsum.photos/200/300"
+                        alt=""
+                     />
+                     <img
+                        className={`${styles["media_content"]}`}
+                        src="https://picsum.photos/200/300"
+                        alt=""
+                     />
+                  </div>
+               </div>
+            </div>
+            <Button btnContent={'Logout'} />
+         </div>
       </>
    );
 }
