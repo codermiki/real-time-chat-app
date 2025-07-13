38fb1e46b403b0ea21a40ca56ff96ab29e3cfc67
codermiki <mikiastadessemiki@gmail.com>
2025-07-13 22:46:36 +0300

    feat: create Button component with styling and props for dynamic content

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import styles from "./Button.module.css";
 
-function Button() {
+function Button({ btnContent }) {
    return (
       <>
-         <div>Button</div>
+         <button className={`${styles["button"]}`}>{btnContent}</button>
       </>
    );
 }
