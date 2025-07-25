a10a042ec5c462daf21ae18efd6fc904008c9def
codermiki <mikiastadessemiki@gmail.com>
2025-07-25 22:21:41 +0300

    fix: delete unnecessary file and install dependency's

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from "./Home.module.css";
+import ChatCard from "../../components/ChatCard/ChatCard";
+import files from "../../components/ChatCard/mockFile";
+import Header from "../../components/Header/Header";
+import RightSideBar from "../../components/RightSideBar/RightSideBar";
 
 function Home() {
    return (
       <>
-         <div>Home</div>
+         <section className="min-h-screen overflow-y-scroll flex ">
+            <section className="flex-7/12">
+               <Header />
+               <ChatCard chatForm="received" files={files} />
+            </section>
+            <section className="flex-3/12">
+               <RightSideBar />
+            </section>
+         </section>
       </>
    );
 }
